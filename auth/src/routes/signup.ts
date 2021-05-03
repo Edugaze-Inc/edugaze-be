baeb99eef3e97b0a828874fa548ec409d0fdcbf7
HagerDakroury <hagerdakroury@gmail.com>
2021-05-03 20:52:34 +0200

    added email/pass validation on signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,27 +1,43 @@
 import express from "express";
+import { body, validationResult } from "express-validator";
 import { User, makeUser } from "../models/user";
 import { createAccessToken } from "../utils/authToken";
 
 const router = express.Router();
 
-router.post("/api/users/signup", async (req, res) => {
-  // TODO validation with Joi
-  const { email, password } = req.body;
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email not Valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 16 })
+      .withMessage("Password must be between 4 and 16 characters"),
+  ],
+  async (req: any, res: any) => {
+    const errors = validationResult(req);
 
-  // see if a user exists
-  if (await User.findOne({ email })) {
-    return res.send("Account Already exists!");
+    if (!errors.isEmpty()) {
+      return res.send("Invalid Email or Password!");
+    }
+
+    const { email, password } = req.body;
+
+    // see if a user exists
+    if (await User.findOne({ email })) {
+      return res.send("Account Already exists!");
+    }
+
+    // making a new user and saving them into the database
+    const newUser = await makeUser({ email, password });
+    await newUser.save();
+
+    // consider user signed in
+    const userJWT = createAccessToken({ email });
+
+    res.setHeader("Authentication", `bearer ${userJWT}`);
+    return res.status(201).send(newUser);
   }
-
-  // making a new user and saving them into the database
-  const newUser = await makeUser({ email, password });
-  await newUser.save();
-
-  // consider user signed in
-  const userJWT = createAccessToken({ email });
-
-  res.setHeader("Authentication", `bearer ${userJWT}`);
-  return res.status(201).send(newUser);
-});
+);
 
 export { router as signupRouter };
