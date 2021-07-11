c09eb738d46ded03f16963125059a3250529fea2
Hager Eldakroury <hagerdakroury@gmail.com>
2021-07-11 23:31:49 +0200

    Add analysis service

--- analysis/index.ts
+++ analysis/index.ts
@@ -1,12 +1,55 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = 4004;
+const server = app.listen(port, () => {
+  console.log("listening for requests on port 4004,");
+});
+var socket = require("socket.io");
+let io = socket(server);
+
 app.get("/", (req, res) => {
-  res.send("<h1>Hello Edugaze!!</h1>");
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Example app listening at http://localhost:${port}`);
+var emotions: { [k: string]: { [k: string]: number } } = {};
+
+io.on("connection", (socket: any) => {
+  console.log("a user connected");
+  socket.on("join", function (data: { sid: string }) {
+    socket.join(data.sid);
+    if (!(data.sid in emotions)) {
+      emotions[data.sid] = {
+        angry: 0,
+        happy: 0,
+        neutral: 0,
+        sad: 0,
+        fearful: 0,
+        disgusted: 0,
+        surprised: 0,
+        out: 0,
+      };
+    }
+    emotions[data.sid]["out"]++;
+  });
+
+  socket.on("chat message", (data: { msg: string; id: string }) => {
+    var prev = data.msg.split("-")[0];
+    var curr = data.msg.split("-")[1];
+    emotions[data.id][prev]--;
+    emotions[data.id][curr]++;
+    console.log(emotions[data.id]);
+
+    io.to(data.id + "-i").emit(
+      "chat message",
+      JSON.stringify(emotions[data.id])
+    );
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
