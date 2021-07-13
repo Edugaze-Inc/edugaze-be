46deab86008ae70560cb88b0c8e869629caf4e43
Ziyad Khalil <ziyadhkhalil@gmail.com>
2021-07-13 22:10:05 +0200

    auth db name

--- auth/src/models/connection.ts
+++ auth/src/models/connection.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const uri = "mongodb://auth-mongo:27017/auth-test";
+const uri = "mongodb://auth-db-service:27017/auth-test";
 
 const connectDb = () => mongoose.connect(uri);
 
