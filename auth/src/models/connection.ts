a95bfb9a49d3f4930b22c29741412eeac40682fe
Hager Eldakroury <hagerdakroury@gmail.com>
2021-07-15 01:46:18 +0200

    fix(auth): Correct database uri

--- auth/src/models/connection.ts
+++ auth/src/models/connection.ts
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
 
-const uri =
-  "mongodb+srv://hager:hagercontactsapp@cluster0.i4gfy.mongodb.net/contactsapp?retryWrites=true&w=majority";
-
+const uri = "mongodb://auth-db-service:27017/auth-test";
 const connectDb = () => mongoose.connect(uri);
 
 export { connectDb };
