c4b472b047c7d5eb2da24ea768e1bba4e5800343
Ziyad Khalil <ziyadhkhalil@gmail.com>
2021-07-17 09:21:02 +0200

    reverted

--- auth/index.ts
+++ auth/index.ts
@@ -6,8 +6,6 @@ import { signupRouter } from "./src/routes/signup";
 import { loginRouter } from "./src/routes/login";
 import { verifyRouter } from "./src/routes/verify";
 import { connectDb } from "./src/models/connection";
-import https from "https";
-import http from "http";
 
 const app = express();
 connectDb().then(() => {
@@ -25,14 +23,9 @@ app.use(loginRouter);
 
 app.use(verifyRouter);
 
-const port = 443;
+const port = 4002;
 
-// app.listen(port, () => {
-//   // eslint-disa ble-next-line no-console
-//   console.log(`Auth app listening at http://localhost:${port}`);
-// });
-
-https.createServer(app).listen(port);
-http.createServer(app).listen(8080);
-
-// https.createServer({}, app).listen(80);
+app.listen(port, () => {
+  // eslint-disa ble-next-line no-console
+  console.log(`Auth app listening at http://localhost:${port}`);
+});
